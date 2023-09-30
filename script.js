8e7737b72628a5d87ed76813608b62fb35079283
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-09-30 13:39:46 -0700

    added pseudocode and framework based off of it

---
+++ script.js
@@ -0,0 +1,44 @@
+//variable declaritions
+var timeLeft = "";
+var userAnswer = "";
+var Q1 = [];
+var Q2 = [];
+var Q3 = [];
+var Q4 = [];
+var Q5 = [];
+var Q6 = [];
+var Q7 = [];
+var Q8 = [];
+var Q9 = [];
+var Q10 = [];
+    //timeRemaining
+//querySelectors
+var button = document.querySelector("#button");
+var questionBlock = document.querySelector();
+var timer = document.querySelector();
+var highScors = document.querySelector();
+//function to start the game
+function startGame(){
+    //listen for button start
+    //start timer in intervals of 1000 ms
+};
+//first question displays and corresponding answers
+    //carousel-box like array to keep track of what question you are on
+//use eventlisteners on buttons check for answers
+    //compare clicked button to what we have as correct answer
+    //if wrong display WRONG!!! and decrement an amount of time from timer
+    //if correct display CORRECT!!!
+    //in either case move onto next question
+    //if end of questions end game
+//repeat until timer hits 0 or run out of questions
+    //when our array of questions returns undefined or length-1 for index
+    //end game
+//Display of time remaining as score
+//end game
+function endGame(){
+    //stop timer
+    //prompt for their initials
+    //save score into high score
+    //clear time
+}
+button.addEventListener("click", startGame);
